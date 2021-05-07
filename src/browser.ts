a5d1ef9a36f8522d98926bf471668c5aa694fd36
tsyzthh <571620158@qq.com>
2021-05-07 17:25:02 +0800

    (feat)/docs - add doc script and CI flow

--- src/browser.ts
+++ src/browser.ts
@@ -1,3 +1,7 @@
+/**
+ * This file is the entrypoint of browser builds.
+ * The code executes when loaded in a browser.
+ */
 import { foo } from './main';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
